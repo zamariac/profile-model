6e1a175264dd5a546b2c2353b117a5ed8079fef0
Aaron Larner <anlarner@gmail.com>
2015-06-07 11:02:26 -0500

    Initial commit

---
+++ dist/scripts/main.js
@@ -0,0 +1,18 @@
+var user = new UserModel();
+var App = Backbone.Router.extend({
+	routes: {
+		'': 'profile',
+		'edit': 'edit'
+	},
+	profile: function() {
+		$('.page').hide();
+		$('#profile').show();
+	},
+	edit: function() {
+		$('.page').hide();
+		$('#edit').show();
+	}
+});
+
+var app = new App();
+Backbone.history.start();
